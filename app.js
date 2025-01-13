8b8f7db6c994d80995e3aa9e447f16ccd218d077
Rahul Yadav <099rahul.yadav@gmail.com>
2025-01-13 23:14:45 +0530

    recent

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const http=require('http')
-const server=http.createServer((req,res)=>{
-    // res.end('Hello World')
-    // console.log(req.url);
-    if(req.url=="/"){
-        res.end('This is Home page')
-    }
-    if(req.url=="/about"){
-        res.end('This is about page')
-    }
-    if(req.url=="/profile"){
-        res.end('This is profile page')
-    }
-    if(req.url=="/contact"){
-        res.end('This is contact page')
-    }
-})
+// const http=require('http')
+// const server=http.createServer((req,res)=>{
+//     // res.end('Hello World')
+//     // console.log(req.url);
+//     if(req.url=="/"){
+//         res.end('This is Home page')
+//     }
+//     if(req.url=="/about"){
+//         res.end('This is about page')
+//     }
+//     if(req.url=="/profile"){
+//         res.end('This is profile page')
+//     }
+//     if(req.url=="/contact"){
+//         res.end('This is contact page')
+//     }
+// })
 
-server.listen(3000);
+// server.listen(3000);
