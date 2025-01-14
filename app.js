5c19f679cb559f4b5c609bd589ed92fb3baed577
Rahul Yadav <099rahul.yadav@gmail.com>
2025-01-14 17:44:35 +0530

    14-01-25

--- app.js
+++ app.js
@@ -21,9 +21,16 @@
 
 const express=require('express');
 const app=express();
+const morgan =require('morgan')
+const dbConnection = require('./config/db')
+const userModel =require('./models/user')
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use(express.json())
 app.set("view engine",'ejs');
+app.use(morgan('dev'))
 app.use(((req,res,next)=>{
-    console.log("this is middleware");
+    // console.log("this is middleware");
     return next();
 }))
 app.get('/',(req,res)=>{
@@ -34,5 +41,48 @@ app.get('/about',(req,res)=>{
 })
 app.get('/profile',(req,res)=>{
     res.send('This is profile page');
+})
+app.post('/get-form-data',(req,res)=>{
+    const {username,email,password}=req.body;
+    console.log(username,email,password);
+    res.send('form data recieved');
+})
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
+//! Create a data in database => C
+app.post('/register', async(req,res)=>{
+    const {username,email,password}=req.body;
+    const newUser=await userModel.create({
+        username:username,
+        email:email,
+        password:password
+    })
+    res.send(newUser)
+})
+
+//! Read data from databade => R
+app.get('/get-users',(req,res)=>{
+    // userModel.find({username:"Rahul Yadav"}).then((users)=>{
+    //     res.send(users)
+    // })
+    userModel.findOne().then((user)=>{
+        res.send(user)
+    })
+})
+
+//! Update data from databade => U
+app.get('/update',async(req,res)=>{
+    await userModel.findOneAndUpdate({username:"Rahul Yadav"},{
+        email:"987t6@gmail.com"
+    })
+    res.send("user-updated")
+})
+
+//! Delete data from databade => D
+
+app.get('/delete-user',async(req,res)=>{
+    await userModel.findOneAndDelete({username:"Rahul Yadav"})
+    res.send("user deleted")
 })
 app.listen(3000)
---
+++ config/db.js
@@ -0,0 +1,9 @@
+//used to connect to database
+
+const mongoose=require('mongoose')
+
+const connection =mongoose.connect('mongodb://0.0.0.0/backend').then(()=>{
+    console.log('connected to database');
+})
+
+module.exports=connection
